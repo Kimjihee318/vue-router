2fb7133e2bc06ba7d4066f09231d65a5b3e24850
jiheekim <jiheeKim318@gmail.com>
2019-03-14 00:46:21 +0900

    Study vue router

--- src/router.ts
+++ src/router.ts
@@ -20,6 +20,22 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/come',
+      name: 'come',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "come" */ './views/Come.vue')
+    },
+    {
+      path: '/to',
+      name: 'to',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "to" */ './views/To.vue')
     }
   ]
 })
---
+++ src/views/Come.vue
@@ -0,0 +1,15 @@
+<template>
+ <div class="come">
+   <h1>Come</h1>
+ </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+</style>
